ab3b42c909592f7339b784d8d13f90b827414f6a
Filipe3405 <filipeacmb@gmail.com>
2025-06-18 17:18:59 -0300

    Create desafio018.py

---
+++ desafio018.py
@@ -0,0 +1,4 @@
+import math
+x = (int(input('Digite um ângulo: ')))
+rad = math.radians(x)
+print(f'o valor do cosseno de {x} é de {math.cos(rad):.2f}, o valor do seu seno é de {math.sin(rad):.2f} e o valor de sua tangente é de {math.tan(rad):.2f}')
