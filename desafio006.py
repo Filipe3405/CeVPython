0d966fbec6b4d348f8a67d318a9679d683238e8a
Filipe3405 <filipeacmb@gmail.com>
2025-06-18 08:34:02 -0300

    Update desafio006.py

--- desafio006.py
+++ desafio006.py
@@ -1,6 +1,2 @@
 n = int(input('digite um valor: '))
-d = n*2
-t = n*3
-r = n**0.5
-print("O dobro do valor{} é {}, seu triplo é {} e sua raiz quadrada é {}".format(n,d,t,r))
-print(f"O dobro do valor {n} é {d}, o seu triplo é {t} e sua raiz quadrada é {r}")
+print(f"O dobro do valor {n} é {n*2}, o seu triplo é {n*3} e sua raiz quadrada é {n**0.5:.3f}")
