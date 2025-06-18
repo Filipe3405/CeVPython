55e45689386c08c41a1e6ba8199320a5992b1d04
Filipe3405 <filipeacmb@gmail.com>
2025-06-18 09:02:04 -0300

    Create desafio003.py

---
+++ desafio003.py
@@ -0,0 +1,8 @@
+pri = int(input('Digite o primeiro número:'))
+sec = int(input('Digite o segundo número: '))
+#soma = pri + sec
+# print("A soma é", soma)
+# print('A soma entre',pri,"e",sec,"vale",soma)
+# print('A soma vale {}'.format(soma))
+#print("A soma entre {0} e {1} vale {2}".format(pri, sec, soma))
+print(f'A soma entre {pri} e {sec} vale {pri+sec}')
