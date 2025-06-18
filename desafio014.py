b98cb55b1245dc9294dab89b503ff6f87597abb1
Filipe3405 <filipeacmb@gmail.com>
2025-06-18 08:58:21 -0300

    Create desafio014.py
    Transformador de temperaturas

---
+++ desafio014.py
@@ -0,0 +1,5 @@
+celsius = float(input('Digite a temperatura em °C: '))
+print(f'O valor da temperatura em fahrenheit é de {(9*celsius/5)+32:.2f}°f')
+
+f = float(input('Digite a temperatura em °f: '))
+print(f'A temperatura {f} em graus celsius é de {5*(f-32)/9} °C')
