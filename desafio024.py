6ec004f6724d657d638f1ffac0fdf0353bcca674
Filipe3405 <filipeacmb@gmail.com>
2025-06-19 19:47:14 -0300

    Update desafio024.py

--- desafio024.py
+++ desafio024.py
@@ -1,7 +1,7 @@
 #Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
-cidade = input('Digite o nome da sua cidade: ')
-city = cidade.title()
-print('Santo' in city)
+cidade = input('Digite o nome da sua cidade: ').title()
+city = cidade.split()
+print('Santo' in city[0])
 # Outra resolução com condicionais 
 cidade = input('Digite o nome da sua cidade: ')
 frstname = cidade.split()
