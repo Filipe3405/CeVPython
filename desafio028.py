1dea0d0fadc7f068394a494254180df44ff571be
Filipe3405 <filipeacmb@gmail.com>
2025-06-20 08:05:21 -0300

    Create desafio028.py

---
+++ desafio028.py
@@ -0,0 +1,11 @@
+#Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+# Computador "pensando" em um número
+import random
+num = random.randrange(0,5)
+# Jogador escolhendo um número
+jog = int(input("Digite um número: "))
+# Comparando os números do computador com o do jogador
+if jog == num:
+    print('você acertou! :D')
+else:
+    print("Você errou! :(")
