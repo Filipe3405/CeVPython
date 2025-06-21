9dec3c95ab9284e74f8246f29b0c719a2101c0f1
Filipe3405 <filipeacmb@gmail.com>
2025-06-21 12:07:33 -0300

    Update desafio039.py

--- desafio039.py
+++ desafio039.py
@@ -3,8 +3,8 @@ from datetime import date
 atual = date.today().year
 ano = int(input('Digite seu ano de nascimento: '))
 if atual - ano == 18:
-    print('É hora de se alistar')
+    print('É hora de se alistar: https://alistamento.eb.mil.br/alistamento')
 elif atual - ano < 18:
-    print('Você é jovem demais para se alistar')
+    print(f'Você é jovem demais para se alistar, ainda faltam {18 - (atual - ano)} anos para você se alistar')
 else:
-     print('Você já se alistou ou já deveria ter se alistado')
+     print(f'Você já se alistou ou já deveria ter se alistado. Já se passaram {(atual-ano)-18} anos do seu alistamento')
