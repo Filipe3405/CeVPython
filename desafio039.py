d4da09a286f01918bf5593e416e1d35c335e3356
Filipe3405 <filipeacmb@gmail.com>
2025-06-21 12:03:31 -0300

    Create desafio039.py

---
+++ desafio039.py
@@ -0,0 +1,10 @@
+# Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+from datetime import date
+atual = date.today().year
+ano = int(input('Digite seu ano de nascimento: '))
+if atual - ano == 18:
+    print('É hora de se alistar')
+elif atual - ano < 18:
+    print('Você é jovem demais para se alistar')
+else:
+     print('Você já se alistou ou já deveria ter se alistado')
